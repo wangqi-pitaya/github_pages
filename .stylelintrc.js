b4f3959de5ee1787afb2421a363e2ee79ed545d3
wangqi-pitaya <873790387@qq.com>
2022-04-05 19:45:35 +0800

    add lint

---
+++ .stylelintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "extends": "stylelint-config-standard",
+  "rules": {
+    "declaration-colon-newline-after": "always-multi-line",
+    "value-list-comma-newline-after": "always-multi-line",
+    "string-quotes": "single",
+    "font-family-name-quotes": "always-unless-keyword"
+  }
+}
