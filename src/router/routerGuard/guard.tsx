ed9daca4d6a716532e306f3b327156cf260b1644
wang-pitaya <wangqi_pitaya@163.com>
2022-04-14 23:46:23 +0800

    fix: build

--- src/router/routerGuard/guard.tsx
+++ src/router/routerGuard/guard.tsx
@@ -1,5 +1,4 @@
 import { Navigate, useLocation, useNavigate } from 'react-router-dom';
-import { useTitle } from 'ahooks';
 
 let temp: any = null;
 
@@ -8,7 +7,7 @@ export default function ({ element, meta, handleRouteBefore }: any) {
 	const location = useLocation();
 	const { pathname }: any = location;
 	const navigate: any = useNavigate();
-	useTitle(meta?.title);
+	document.title = meta?.title
 
 	if (handleRouteBefore) {
 		if (temp === element) return element;
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: '/web_space/',
+	base: '/vite-react-template/',
 	plugins: [react()],
 	css: {
 		preprocessorOptions: {
